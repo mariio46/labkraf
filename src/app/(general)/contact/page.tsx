f8d4427ec6afff446513e8aef9debd59b61f893f
Mario <mariomad2296@gmail.com>
2023-12-17 15:06:05 +0800

    wip

--- src/app/(general)/contact/page.tsx
+++ src/app/(general)/contact/page.tsx
@@ -1,3 +1,75 @@
+import { Container } from '@/components/container';
+import { Header } from '@/components/header';
+import { PrimaryHeading, SecondaryHeading } from '@/components/headings';
+import { Icon } from '@/components/icon';
+import { TooltipInfo } from '@/components/tooltip-info';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
+import Link from 'next/link';
+
 export default function Contact() {
-    return <div>Contact</div>;
+    return (
+        <>
+            <Header>
+                <SecondaryHeading>Contact</SecondaryHeading>
+                <PrimaryHeading className='my-5 max-w-4xl text-4xl text-brand'>
+                    Ayo hubungi kami dan diskusikan apa yang anda inginkan.
+                </PrimaryHeading>
+            </Header>
+            <Container className='mt-10 flex w-full max-w-none items-start gap-6'>
+                <section id='contact-form' className='w-1/2 space-y-3'>
+                    <div>
+                        <Label htmlFor='name'>Name</Label>
+                        <Input type='text' className='mt-1' id='name' />
+                    </div>
+                    <div>
+                        <Label htmlFor='phone'>Nomor Hp</Label>
+                        <Input type='text' className='mt-1' id='phone' />
+                    </div>
+                    <div>
+                        <Label htmlFor='email'>Email</Label>
+                        <Input type='email' className='mt-1' id='email' />
+                    </div>
+                    <div>
+                        <Label htmlFor='needs'>Kebutuhan Anda</Label>
+                        <Textarea className='mt-1 h-28' id='needs' />
+                    </div>
+                    <div>
+                        <Button type='button' className='bg-brand hover:bg-[#EAD301]'>
+                            Kirim
+                        </Button>
+                    </div>
+                </section>
+                <section id='links' className='w-1/2'>
+                    <div className='flex flex-wrap items-center gap-3'>
+                        <TooltipInfo content='Email'>
+                            <Link href='mailto:office@labkraf.id' className='rounded-full bg-accent p-5'>
+                                <Icon name='IconMail' className='h-8 w-8 stroke-[1.5] text-muted-foreground' />
+                            </Link>
+                        </TooltipInfo>
+                        <TooltipInfo content='Nomor Telepon'>
+                            <Link
+                                href='https://wa.me/6285398069499'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                className='rounded-full bg-accent p-5'>
+                                <Icon name='IconBrandWhatsapp' className='h-8 w-8 stroke-[1.5] text-muted-foreground' />
+                            </Link>
+                        </TooltipInfo>
+                        <TooltipInfo content='Alamat'>
+                            <Link
+                                href='https://wa.me/6285398069499'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                className='rounded-full bg-accent p-5'>
+                                <Icon name='IconMapPin' className='h-8 w-8 stroke-[1.5] text-muted-foreground' />
+                            </Link>
+                        </TooltipInfo>
+                    </div>
+                </section>
+            </Container>
+        </>
+    );
 }
--- src/app/(general)/services/page.tsx
+++ src/app/(general)/services/page.tsx
@@ -13,6 +13,7 @@ export default function Service() {
             </Header>
             <LabUrben />
             <LabApps />
+            <LabGraph />
         </>
     );
 }
@@ -23,19 +24,13 @@ const LabUrben = () => {
             <div className='grid grid-cols-1 gap-5 2xl:grid-cols-2 2xl:gap-10'>
                 <div className='col-auto flex h-full flex-col justify-center gap-4'>
                     <SecondaryHeading className='inline-flex items-center gap-2 text-brand'>
-                        <Image
-                            src='/images/lab-urben.png'
-                            alt='Lab Urben'
-                            width={2560}
-                            height={1706}
-                            className='h-7 w-7 rounded-lg bg-white'
-                        />
+                        <Icon name='IconStar' className='h-7 w-7 stroke-2 text-brand' />
                         Lab Urben
                     </SecondaryHeading>
                     <PrimaryParagraph className='font-medium text-primary/80 lg:text-base'>
                         Architecture & Urban Design
                     </PrimaryParagraph>
-                    <div className='flex justify-start gap-6'>
+                    <div className='flex flex-col justify-start gap-1.5 2xl:flex-row 2xl:gap-3'>
                         <SecondaryParagraph className='flex flex-col gap-1.5'>
                             <ServiceOfferItem value='Master Plan' />
                             <ServiceOfferItem value='Interior Design' />
@@ -52,12 +47,12 @@ const LabUrben = () => {
                 </div>
                 <div className='col-auto'>
                     <Image
-                        src={'/images/about-1.png'}
-                        alt='Tentang Safoto 1'
+                        src={'/images/lab-urben.png'}
+                        alt='Lab Urben Service'
                         width={551}
                         height={830}
                         loading='lazy'
-                        className='aspect-video h-full w-full rounded-lg object-cover'
+                        className='aspect-video h-full w-full rounded-lg bg-white object-cover'
                     />
                 </div>
             </div>
@@ -71,29 +66,23 @@ const LabApps = () => {
             <div className='grid grid-cols-1 gap-5 2xl:grid-cols-2 2xl:gap-10'>
                 <div className='order-last col-auto 2xl:order-first'>
                     <Image
-                        src={'/images/about-2.png'}
-                        alt='Tentang Safoto 1'
+                        src={'/images/lab-apps.png'}
+                        alt='Lab Apps Service'
                         width={551}
                         height={830}
                         loading='lazy'
-                        className='aspect-video h-full w-full rounded-lg object-cover'
+                        className='aspect-video h-full w-full rounded-lg bg-zinc-700 object-cover'
                     />
                 </div>
                 <div className='order-first col-auto flex h-full flex-col justify-center gap-4 2xl:order-last'>
                     <SecondaryHeading className='inline-flex items-center gap-2 text-brand'>
-                        <Image
-                            src='/images/lab-apps.png'
-                            alt='Lab Apps'
-                            width={2560}
-                            height={1706}
-                            className='h-7 w-7 rounded-lg bg-white'
-                        />
+                        <Icon name='IconStar' className='h-7 w-7 stroke-2 text-brand' />
                         Lab Apps
                     </SecondaryHeading>
                     <PrimaryParagraph className='font-medium text-primary/80 lg:text-base'>
                         Application Development & Digital Solution
                     </PrimaryParagraph>
-                    <div className='flex flex-col-reverse justify-start gap-3 2xl:flex-row'>
+                    <div className='flex flex-col-reverse justify-start gap-1.5 2xl:flex-row 2xl:gap-3'>
                         <SecondaryParagraph className='flex flex-col gap-1.5'>
                             <ServiceOfferItem value='E-Goverment' />
                             <ServiceOfferItem value='UI / UX Design' />
@@ -107,6 +96,48 @@ const LabApps = () => {
                             <ServiceOfferItem value='Insfratruktur Management' />
                         </SecondaryParagraph>
                     </div>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+const LabGraph = () => {
+    return (
+        <Container className='mt-16 w-full max-w-none 2xl:my-40'>
+            <div className='grid grid-cols-1 gap-5 2xl:grid-cols-2 2xl:gap-10'>
+                <div className='col-auto flex h-full flex-col justify-center gap-4'>
+                    <SecondaryHeading className='inline-flex items-center gap-2 text-brand'>
+                        <Icon name='IconStar' className='h-7 w-7 stroke-2 text-brand' />
+                        Lab Graph
+                    </SecondaryHeading>
+                    <PrimaryParagraph className='font-medium text-primary/80 lg:text-base'>
+                        Digital Marketing & Visual Art
+                    </PrimaryParagraph>
+                    <div className='flex flex-col justify-start gap-1.5 2xl:flex-row 2xl:gap-3'>
+                        <SecondaryParagraph className='flex flex-col gap-1.5'>
+                            <ServiceOfferItem value='Brand Identity' />
+                            <ServiceOfferItem value='Social Media Management' />
+                            <ServiceOfferItem value='Social Media Analyst' />
+                            <ServiceOfferItem value='Video & Motion Graphic' />
+                        </SecondaryParagraph>
+                        <SecondaryParagraph className='flex flex-col gap-1.5'>
+                            <ServiceOfferItem value='Documentation' />
+                            <ServiceOfferItem value='Digital Campaign' />
+                            <ServiceOfferItem value='Digital Feature' />
+                            <ServiceOfferItem value='Business Plan Management' />
+                        </SecondaryParagraph>
+                    </div>
+                </div>
+                <div className='col-auto'>
+                    <Image
+                        src={'/images/lab-graph.png'}
+                        alt='Lab Graph Service'
+                        width={551}
+                        height={830}
+                        loading='lazy'
+                        className='aspect-video h-full w-full rounded-lg bg-[#EAD301] object-cover'
+                    />
                 </div>
             </div>
         </Container>
